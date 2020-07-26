c6232cd2972049a9d72f18a60c5fc626e03bd64a
Luk1Pr0 <lpytel16@gmail.com>
2020-07-26 10:53:47 +0100

    Updates

---
+++ precache-manifest.ec035903f08c5fad3e0b99f0935f06b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d60b786c52a4fd67eb2222394714cd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "41fba569857e8bc2e643",
+    "url": "/robofriends/static/css/main.7a802ef9.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41fba569857e8bc2e643",
+    "url": "/robofriends/static/js/main.2a5d038a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
